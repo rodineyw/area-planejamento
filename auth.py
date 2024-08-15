17945ec918312eac402178ee1b6d1c88af33bad5
rodineyw <rodineyw@yahoo.com.br>
2024-08-15 16:33:52 -0300

    feat: limitação de acesso de usuário e admin

--- auth.py
+++ auth.py
@@ -1,3 +1,4 @@
+''' Autenticação '''
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -7,23 +8,27 @@ load_dotenv()
 
 # Dicionário de usuários e senhas usando as variáveis do .env
 USER_CREDENTIALS = {
-    os.getenv("USER_1"): os.getenv("PASSWORD_1"),
-    os.getenv("USER_2"): os.getenv("PASSWORD_2")
+    os.getenv("USER_1"): {"password": os.getenv("PASSWORD_1"), "role": "visitor", },
+    os.getenv("USER_2"): {"passwordo": os.getenv("PASSWORD_2"), "role": "admin"}
 }
 
 
 # Função de autenticação
 
 def login():
+    ''' Funcionalidade de login '''
     st.sidebar.title("Autenticação")
     username = st.sidebar.text_input("Usuário")
     password = st.sidebar.text_input("Senha", type="password")
     if st.sidebar.button("Login"):
-        if USER_CREDENTIALS.get(username) == password:
+        user = USER_CREDENTIALS.get(username)
+        if user and user["password"] == password:
             st.session_state["authenticated"] = True
+            st.session_state["username"] = username
+            st.session_state["role"] = user["role"]
             st.sidebar.success("Login bem-sucedido!")
         else:
             st.sidebar.error("Usuário ou senha incorretos.")
 
         # Depuração: Mostrar se o login foi bem-sucedido
-        st.write("Autenticação após login:", st.session_state["authenticated"])
+        # st.write("Autenticação após login:", st.session_state["authenticated"])
